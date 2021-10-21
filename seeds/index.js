969bdd07d68284c3518668adfc3e0f4b7e943ef0
knight <knight.sarai.dev@gmail.com>
2021-10-21 20:30:54 +0300

    Fix DB Auther issue

--- seeds/index.js
+++ seeds/index.js
@@ -12,23 +12,25 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
+db.once("open", () =>
+{
     console.log("Database connected");
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
-const seedDB = async () => {
+const seedDB = async () =>
+{
     await Campground.deleteMany({});
     for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //YOUR USER ID
-            author: '60e1720b0b3f44ad9f19c4fc',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
+            author: "61719c1b9596ed2c473bcd3d",
+            location: `${ cities[random1000].city }, ${ cities[random1000].state }`,
+            title: `${ sample(descriptors) } ${ sample(places) }`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
             geometry: {
@@ -53,6 +55,7 @@ const seedDB = async () => {
     }
 }
 
-seedDB().then(() => {
+seedDB().then(() =>
+{
     mongoose.connection.close();
 })
